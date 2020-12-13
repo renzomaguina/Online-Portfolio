708b532323725398ad43726483d9ac14013234e2
BigDoggThePunisher <30597215+BigDoggThePunisher@users.noreply.github.com>
2020-12-13 13:55:55 -0500

    First commit Message

--- C #/FlickerViewer/FlickerViewer/FlickerViewer/FlickrResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlickerViewer
-{
-    internal class FlickrResult
-    {
-        public string flickrURL { get; set; }
-        public string title { get; set; }
-        public string URL { get; internal set; }
-    }
-}
